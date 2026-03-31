a918622e11c3806ced33f0cb93a3dd1617cabab2
Adeyemi Abiade <abiadeabdulazeez@gmail.com>
2026-03-31 13:38:22 +0100

    (chore): register deps in program.cs and setup docker

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Caesura.Api.Controllers;
+
+public class AuthController: ControllerBase
+{
+    
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,89 @@
+using System.Text;
+using Caesura.Api.Helpers;
 using Caesura.Api.Middleware;
+using FluentValidation;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("Default");
+var jwtSecret = builder.Configuration["Jwt:Secret"] ??
+                throw new InvalidOperationException("Jwt:Secret is not configured");
+
 
 // Add services to the container.
-
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSecret))
+        };
+    });
+builder.Services.AddAuthorization();
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddCors(options => options.AddPolicy("Frontend", policy =>
+{
+    var origins = builder.Configuration
+        .GetSection("AllowedOrigins")
+        .Get<String[]>() ?? ["http://localhost:3000"];
+
+    policy.WithOrigins(origins)
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+}));
+
+
+// SERVICES
+// builder.Services.AddScoped<IAuthService, AuthService>();
+// builder.Services.AddScoped<BooksService>();
+
+// Register fluent validator
+// builder.Services.AddValidatorsFromAssemblyContaining<RegisterRequestValidator>();
+
+// Disable the built-in [Required] / data annotation validation filter
+// so FluentValidation is the single source of truth for all validation errors
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+    options.SuppressModelStateInvalidFilter = true);
+
+builder.Services.AddControllers()
+    .AddJsonOptions(opts =>
+    {
+        opts.JsonSerializerOptions.PropertyNamingPolicy = new SnakeCaseNamingPolicy();
+        opts.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+
+        // Skip null fields entirely in res
+        opts.JsonSerializerOptions.DefaultIgnoreCondition =
+            System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull;
+        opts.JsonSerializerOptions.Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping;
+    });
+
+// SWagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Caesura API", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header
+    });
+  
+});
 
 var app = builder.Build();
 
@@ -18,8 +95,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("Frontend");
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
